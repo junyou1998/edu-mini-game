413a6d105979ce86c9c9497ef69319dd4bcafa9b
陳俊佑 <35156817+junyou1998@users.noreply.github.com>
2022-08-13 22:40:46 +0800

    match game

---
+++ js/match game.js
@@ -0,0 +1,206 @@
+const brands1 = [
+    {
+      iconName: "adobe",
+      brandName: "Adobe",
+      color: "#ff0000"
+    },
+    {
+      iconName: "airbnb",
+      brandName: "Airbnb",
+      color: "#fd5c63"
+    },
+    {
+      iconName: "amazon",
+      brandName: "Amazon",
+      color: "#333333"
+    },
+    {
+      iconName: "android",
+      brandName: "Android",
+      color: "#a4c639"
+    },
+    {
+      iconName: "angellist",
+      brandName: "AngelList",
+      color: "#000000"
+    }
+  ];
+  const brands = [
+    {
+      iconName: "下龍灣",
+      titleViet: "下龍灣",
+      color: "#ff0000"
+    },
+    {
+      iconName: "文廟",
+      titleViet: "文廟",
+      color: "#fd5c63"
+    },
+    {
+      iconName: "水上木偶戲",
+      titleViet: "水上木偶戲",
+      color: "#333333"
+    },
+    {
+      iconName: "西貢聖母聖殿主教座堂",
+      titleViet: "西貢聖母聖殿主教座堂",
+      color: "#a4c639"
+    },
+    {
+      iconName: "河內",
+      titleViet: "河內",
+      color: "#000000"
+    }
+  ];
+  let correct = 0;
+  let total = 0;
+  const totalDraggableItems = 3;
+  const totalMatchingPairs = 3; // Should be <= totalDraggableItems
+  
+  const scoreSection = document.querySelector(".score");
+  const correctSpan = scoreSection.querySelector(".correct");
+  const totalSpan = scoreSection.querySelector(".total");
+  const playAgainBtn = scoreSection.querySelector("#play-again-btn");
+  
+  const draggableItems = document.querySelector(".draggable-items");
+  const matchingPairs = document.querySelector(".matching-pairs");
+  let draggableElements;
+  let droppableElements;
+  
+  initiateGame();
+  
+  function initiateGame() {
+    const randomDraggableBrands = generateRandomItemsArray(totalDraggableItems, brands);
+    const randomDroppableBrands = totalMatchingPairs<totalDraggableItems ? generateRandomItemsArray(totalMatchingPairs, randomDraggableBrands) : randomDraggableBrands;
+    const alphabeticallySortedRandomDroppableBrands = [...randomDroppableBrands].sort((a,b) => a.titleViet.toLowerCase().localeCompare(b.titleViet.toLowerCase()));
+    
+    // Create "draggable-items" and append to DOM
+    for(let i=0; i<randomDraggableBrands.length; i++) {
+      draggableItems.insertAdjacentHTML("beforeend", `
+
+        <div style="background: url('./assets/match/${randomDraggableBrands[i].iconName}.png')" id="${randomDraggableBrands[i].iconName}" class="draggable" draggable="true"></div>
+      `);
+    }
+    // <i class="fab fa-${randomDraggableBrands[i].iconName} draggable" draggable="true" style="color: ${randomDraggableBrands[i].color};" id="${randomDraggableBrands[i].iconName}"></i>
+
+
+    // Create "matching-pairs" and append to DOM
+    for(let i=0; i<alphabeticallySortedRandomDroppableBrands.length; i++) {
+      matchingPairs.insertAdjacentHTML("beforeend", `
+        <div class="matching-pair">
+          <span class="label">${alphabeticallySortedRandomDroppableBrands[i].titleViet}</span>
+          <span class="droppable" data-brand="${alphabeticallySortedRandomDroppableBrands[i].iconName}"></span>
+        </div>
+      `);
+    }
+    
+    draggableElements = document.querySelectorAll(".draggable");
+    droppableElements = document.querySelectorAll(".droppable");
+    
+    draggableElements.forEach(elem => {
+      elem.addEventListener("dragstart", dragStart);
+      // elem.addEventListener("drag", drag);
+      // elem.addEventListener("dragend", dragEnd);
+    });
+    
+    droppableElements.forEach(elem => {
+      elem.addEventListener("dragenter", dragEnter);
+      elem.addEventListener("dragover", dragOver);
+      elem.addEventListener("dragleave", dragLeave);
+      elem.addEventListener("drop", drop);
+    });
+  }
+  
+  // Drag and Drop Functions
+  
+  //Events fired on the drag target
+  
+  function dragStart(event) {
+    event.dataTransfer.setData("text", event.target.id); // or "text/plain"
+  }
+  
+  //Events fired on the drop target
+  
+  function dragEnter(event) {
+    if(event.target.classList && event.target.classList.contains("droppable") && !event.target.classList.contains("dropped")) {
+      event.target.classList.add("droppable-hover");
+    }
+  }
+  
+  function dragOver(event) {
+    if(event.target.classList && event.target.classList.contains("droppable") && !event.target.classList.contains("dropped")) {
+      event.preventDefault();
+    }
+  }
+  
+  function dragLeave(event) {
+    if(event.target.classList && event.target.classList.contains("droppable") && !event.target.classList.contains("dropped")) {
+      event.target.classList.remove("droppable-hover");
+    }
+  }
+  
+  function drop(event) {
+    event.preventDefault();
+    event.target.classList.remove("droppable-hover");
+    const draggableElementBrand = event.dataTransfer.getData("text");
+    const droppableElementBrand = event.target.getAttribute("data-brand");
+    const isCorrectMatching = draggableElementBrand===droppableElementBrand;
+    total++;
+    if(isCorrectMatching) {
+      const draggableElement = document.getElementById(draggableElementBrand);
+      event.target.classList.add("dropped");
+      draggableElement.classList.add("dragged");
+      draggableElement.setAttribute("draggable", "false");
+      event.target.innerHTML = `<i class="fab fa-${draggableElementBrand}" style="color: ${draggableElement.style.color};"></i>`;
+      correct++;  
+    }
+    scoreSection.style.opacity = 0;
+    setTimeout(() => {
+      correctSpan.textContent = correct;
+      totalSpan.textContent = total;
+      scoreSection.style.opacity = 1;
+    }, 200);
+    if(correct===Math.min(totalMatchingPairs, totalDraggableItems)) { // Game Over!!
+      playAgainBtn.style.display = "block";
+      setTimeout(() => {
+        playAgainBtn.classList.add("play-again-btn-entrance");
+      }, 200);
+    }
+  }
+  
+  // Other Event Listeners
+  playAgainBtn.addEventListener("click", playAgainBtnClick);
+  function playAgainBtnClick() {
+    playAgainBtn.classList.remove("play-again-btn-entrance");
+    correct = 0;
+    total = 0;
+    draggableItems.style.opacity = 0;
+    matchingPairs.style.opacity = 0;
+    setTimeout(() => {
+      scoreSection.style.opacity = 0;
+    }, 100);
+    setTimeout(() => {
+      playAgainBtn.style.display = "none";
+      while (draggableItems.firstChild) draggableItems.removeChild(draggableItems.firstChild);
+      while (matchingPairs.firstChild) matchingPairs.removeChild(matchingPairs.firstChild);
+      initiateGame();
+      correctSpan.textContent = correct;
+      totalSpan.textContent = total;
+      draggableItems.style.opacity = 1;
+      matchingPairs.style.opacity = 1;
+      scoreSection.style.opacity = 1;
+    }, 500);
+  }
+  
+  // Auxiliary functions
+  function generateRandomItemsArray(n, originalArray) {
+    let res = [];
+    let clonedArray = [...originalArray];
+    if(n>clonedArray.length) n=clonedArray.length;
+    for(let i=1; i<=n; i++) {
+      const randomIndex = Math.floor(Math.random()*clonedArray.length);
+      res.push(clonedArray[randomIndex]);
+      clonedArray.splice(randomIndex, 1);
+    }
+    return res;
+  }
